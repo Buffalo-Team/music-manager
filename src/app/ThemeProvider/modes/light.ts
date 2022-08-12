24efafd727a9d3a9d8d9fd82b00ea87905fa75c3
Bartosz Skwara <bartoszskwara95@gmail.com>
2022-08-12 17:46:58 +0200

    Add MUI Theme

---
+++ src/app/ThemeProvider/modes/light.ts
@@ -0,0 +1,17 @@
+const lightTheme = {
+    text: {
+        primary: '#000000',
+    },
+    background: {
+        primary: '#E8E8E8',
+        secondary: '#F9F9F9',
+    },
+    warning: {
+        main: '#E95858',
+    },
+    accent: {
+        main: '#3B6064',
+    },
+};
+
+export default lightTheme;
