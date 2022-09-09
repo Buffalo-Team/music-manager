f651498b7853e573b8c7640de85ac30fb84acbcc
Bartosz Skwara <bartoszskwara95@gmail.com>
2022-09-09 20:40:01 +0200

    Refactor app

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from 'app/store';
 import ThemeProvider from 'app/ThemeProvider';
-import Snackbar from 'components/Snackbar';
 import 'app/translations';
 
 const App = () => (
     <BrowserRouter>
         <Provider store={store}>
             <ThemeProvider>
-                <Snackbar />
-                <p>Without routes</p>
+                <p>Without snackbar</p>
             </ThemeProvider>
         </Provider>
     </BrowserRouter>
