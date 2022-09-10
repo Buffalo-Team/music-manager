f968bafe74965168e34865f15c66927f86a606a4
Bartosz Skwara <bartoszskwara95@gmail.com>
2022-09-10 13:35:13 +0200

    Only routes no theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,12 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from 'app/store';
 import 'app/translations';
-import CurrentUserManager from 'components/CurrentUserManager';
-import Snackbar from 'components/Snackbar';
 import Routes from 'pages/Routes';
 
 const App = () => (
     <BrowserRouter>
         <Provider store={store}>
-            <CurrentUserManager />
-            <Snackbar />
+            <p>Only Routes no theme</p>
             <Routes />
         </Provider>
     </BrowserRouter>
